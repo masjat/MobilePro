1f0ae52ae797cddc09c51aabe6eb89345b00a779
masjat <jatmikaherlambang11@gmail.com>
2024-03-07 16:44:55 +0700

    mengubah nama dan kalimat

--- app/src/main/java/com/example/mobileappp1/MainActivity.kt
+++ app/src/main/java/com/example/mobileappp1/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface (color = Color.Cyan){
         Text(
-            text = "Hello $name! Kamu siapa?",
+            text = "Hello $name! Kamu siapa?, kok ndlogok!!",
             modifier = Modifier.padding(24.dp)
         )
     }
